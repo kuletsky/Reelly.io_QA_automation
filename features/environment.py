9050c43d2a953185fcab196ea258a47a649c4886
kuletsky <kuletsky@gmail.com>
2024-05-27 22:19:49 -0400

    created the project and its structure

---
+++ features/environment.py
@@ -0,0 +1,83 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+
+# from app.application import Application
+from log_files.logger import logger
+
+
+def browser_init(context, scenario_name):
+    """
+    :param scenario_name:
+    :param context: Behave context
+    """
+
+    ## CHROME ###
+    driver_path = ChromeDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Chrome(service=service)
+
+    ### FIREFOX ###
+    # driver_path = GeckoDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Firefox(service=service)
+
+    ### SAFARI ###
+    # context.driver = webdriver.Safari()
+
+    ### HEADLESS MODE ###
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # service = Service(ChromeDriverManager().install())
+    # context.driver = webdriver.Chrome(
+    #     options=options,
+    #     service=service
+    # )
+
+
+    ## BROWSERSTACK ###
+    # bs_user = 'kuletsky_D18EAl'
+    # bs_key = 'cjrUnYqkUDR8xPwnqxXj'
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    # options = Options()
+    # bstack_options = {
+    #     'os': 'Windows',
+    #     'osVersion': '11',
+    #     # 'deviceName': 'iPhone 13',
+    #     'browserName': 'Chrome',
+    #     'sessionName': scenario_name
+    # }
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
+
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(6)
+    context.driver.wait = WebDriverWait(context.driver, 15)
+
+    # context.app = Application(context.driver)
+
+
+def before_scenario(context, scenario):
+    print('\nStarted scenario: ', scenario.name)
+    logger.info(f'Started scenario: {scenario.name}')
+    browser_init(context, scenario.name)
+
+
+def before_step(context, step):
+    print('\nStarted step: ', step)
+    logger.info(f'Started step: {step}')
+
+
+def after_step(context, step):
+    if step.status == 'failed':
+        print('\nStep failed: ', step)
+        logger.warning(f'Step failed: {step}')
+        # context.app.base_page.save_screenshot(step)
+
+
+def after_scenario(context, feature):
+    context.driver.delete_all_cookies()
+    context.driver.quit()
