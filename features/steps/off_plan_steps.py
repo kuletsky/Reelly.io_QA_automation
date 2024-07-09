199c3e8a46fdec6669319b6772d533f24e8d0cc3
kuletsky <kuletsky@gmail.com>
2024-07-08 21:27:46 -0400

    edded new Feature

--- features/steps/off_plan_steps.py
+++ features/steps/off_plan_steps.py
@@ -2,9 +2,8 @@ from selenium.webdriver.common.by import By
 from behave import given, when, then
 
 
-GRID = (By.XPATH, '//a[wized="cardOfProperty"]')
-
-
 @then ('Verify that all projects are shown')
 def verify_all_projects(context):
-    context.app.off_plan_page.wait_until_visible(GRID)
+    context.app.off_plan_page.verify_all_projects()
+    # context.app.off_plan_page.wait_until_visible(GRID)
+
--- pages/off_plan_page.py
+++ pages/off_plan_page.py
@@ -3,4 +3,12 @@ from selenium.webdriver.common.by import By
 
 
 class OffPlanPage(Page):
-    pass
+    GRID = (By.XPATH, '//a[@wized="cardOfProperty"]')
+
+    def verify_all_projects(self):
+        self.wait_until_visible(self.GRID)
+        all_projects = self.find_elements(*self.GRID)
+        print(f'How many elements on the page?: {len(all_projects)}')
+
+        for project in all_projects:
+            assert project
