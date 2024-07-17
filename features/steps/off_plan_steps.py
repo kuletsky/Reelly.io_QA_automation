c07fba3b0a34f8d92a1b69c9310d292db2dfe337
kuletsky <kuletsky@gmail.com>
2024-07-16 20:37:33 -0400

    added test case

--- features/steps/off_plan_steps.py
+++ features/steps/off_plan_steps.py
@@ -2,6 +2,11 @@ from selenium.webdriver.common.by import By
 from behave import given, when, then
 
 
+@when ('Click on top Menu {link_text}')
+def click_on_top_menu(context, link_text):
+    context.app.off_plan_page.click_link_on_off(link_text)
+
+
 @then ('Verify that all projects are shown')
 def verify_all_projects(context):
     context.app.off_plan_page.verify_all_projects()
--- pages/off_plan_page.py
+++ pages/off_plan_page.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 
 class OffPlanPage(Page):
     GRID = (By.CSS_SELECTOR, 'a[wized="cardOfProperty"]')
+    LINK_TEXT = (By.XPATH, '//a[text()={TEXT}]')
 
     def verify_all_projects(self):
         self.wait_until_visible(*self.GRID)
@@ -13,3 +14,10 @@ class OffPlanPage(Page):
         for project in all_projects:
             assert project, f'Error! Projects are not shown'
         print(f'All {len(all_projects)} projects are shown')
+
+    def _get_locator(self, text):
+        return [self.LINK_TEXT[0], self.LINK_TEXT[1].replace('{TEXT}', text)]
+
+    def click_link_on_off(self, link_text):
+        locator = self._get_locator(link_text)
+        self.click(*locator)
