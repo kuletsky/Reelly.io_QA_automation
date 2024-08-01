0ccab3cfb7cb5cf22007f0ec094438e2c2329c96
kuletsky <kuletsky@gmail.com>
2024-07-31 22:24:13 -0400

    added scenario

--- pages/community_page.py
+++ pages/community_page.py
@@ -4,7 +4,9 @@ from pages.base_page import Page
 
 
 class CommunityPage(Page):
-    BTN_CONTACT_SUPPORT = (By.XPATH, By.XPATH, '//div[@class="community-blog"]//div[@class="left-block"]//div[contains(@class, "chat-button") and contains(@class, "w-button")]')
+    BTN_CONTACT_SUPPORT = (By.XPATH, '//a[text()="Contact support"]')
 
     def verify_contact_support_button(self):
-        self.click(*self.BTN_CONTACT_SUPPORT)
+        BTN_SUPPORT = self.find_elements(By.XPATH, self.BTN_CONTACT_SUPPORT)
+        # BTN_SUPPORT[1].click()
+        self.click(*BTN_SUPPORT[1])
