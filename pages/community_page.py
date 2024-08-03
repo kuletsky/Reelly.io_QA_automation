23cbf6526000be5002c9f8f0ea7db7ae048266e7
kuletsky <kuletsky@gmail.com>
2024-08-02 20:44:03 -0400

    added scenario

--- pages/community_page.py
+++ pages/community_page.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-
+from time import sleep
 from pages.base_page import Page
 
 
@@ -7,6 +7,6 @@ class CommunityPage(Page):
     BTN_CONTACT_SUPPORT = (By.XPATH, '//a[text()="Contact support"]')
 
     def verify_contact_support_button(self):
-        BTN_SUPPORT = self.find_elements(By.XPATH, self.BTN_CONTACT_SUPPORT)
-        # BTN_SUPPORT[1].click()
-        self.click(*BTN_SUPPORT[1])
+        BTN_SUPPORT = self.find_elements(*self.BTN_CONTACT_SUPPORT)
+        element = BTN_SUPPORT[1]
+        self.driver.execute_script("arguments[0].click();", element)
