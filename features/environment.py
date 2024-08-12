f54f08729c1f018a4eec1d7cc67e201d30fa332a
kuletsky <kuletsky@gmail.com>
2024-08-12 18:22:02 -0400

    inviranment

--- features/environment.py
+++ features/environment.py
@@ -38,9 +38,9 @@ def browser_init(context, scenario_name):
     """
 
     ### CHROME ###
-    # driver_path = ChromeDriverManager().install()
-    # service = Service(driver_path)
-    # context.driver = webdriver.Chrome(service=service)
+    driver_path = ChromeDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Chrome(service=service)
 
     ### FIREFOX ###
     # driver_path = GeckoDriverManager().install()
@@ -78,27 +78,27 @@ def browser_init(context, scenario_name):
     #                                   options=options)
 
     ### BROWSERSTACK ###
-    bs_user = 'kuletsky_D18EAl'
-    bs_key = 'cjrUnYqkUDR8xPwnqxXj'
-    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-
-    options = Options()
-    bstack_options = {
-        'os': 'OS X',
-        'osVersion': 'Monterey',
-        # 'deviceName': 'Pixel 7',
-        # "realMobile": "true",
-        'browserName': 'Chrome',
-        'sessionName': scenario_name
-    }
-    options.set_capability('bstack:options', bstack_options)
-    context.driver = webdriver.Remote(command_executor=url, options=options)
-
-    context.driver.maximize_window()
-    context.driver.implicitly_wait(6)
-    context.driver.wait = WebDriverWait(context.driver, 15)
-
-    context.app = Application(context.driver)
+    # bs_user = 'kuletsky_D18EAl'
+    # bs_key = 'cjrUnYqkUDR8xPwnqxXj'
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    # options = Options()
+    # bstack_options = {
+    #     'os': 'OS X',
+    #     'osVersion': 'Monterey',
+    #     # 'deviceName': 'Pixel 7',
+    #     # "realMobile": "true",
+    #     'browserName': 'Chrome',
+    #     'sessionName': scenario_name
+    # }
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
+    #
+    # context.driver.maximize_window()
+    # context.driver.implicitly_wait(6)
+    # context.driver.wait = WebDriverWait(context.driver, 15)
+    #
+    # context.app = Application(context.driver)
 
 
 def before_scenario(context, scenario):
