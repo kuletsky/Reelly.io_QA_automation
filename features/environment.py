d01c16341c2610c62238f04d986e2a72ad602e0f
kuletsky <kuletsky@gmail.com>
2024-08-13 20:16:28 -0400

    added new scenario

--- features/environment.py
+++ features/environment.py
@@ -94,11 +94,11 @@ def browser_init(context, scenario_name):
     # options.set_capability('bstack:options', bstack_options)
     # context.driver = webdriver.Remote(command_executor=url, options=options)
     #
-    # context.driver.maximize_window()
-    # context.driver.implicitly_wait(6)
-    # context.driver.wait = WebDriverWait(context.driver, 15)
-    #
-    # context.app = Application(context.driver)
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(6)
+    context.driver.wait = WebDriverWait(context.driver, 15)
+
+    context.app = Application(context.driver)
 
 
 def before_scenario(context, scenario):
---
+++ features/tests/user_guide_tests.feature
@@ -0,0 +1,13 @@
+# Created by costa at 8/13/24
+Feature: Tests for user guide page
+  All testing scenarios for user quide page are located here
+
+  Background:
+    Given Open the main page
+    When Click on the "Open in browser"
+    When Log in to the page
+
+    Scenario: User can open User guide page
+      When Click on "Settings" option at the left side menu
+      And Click on "User Guide"
+      Then Verify that URL of window contains user-guide
